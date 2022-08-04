2973fb74182485b835729fafe9ee124f4f52dfaa
Yacine développeur <yacine.developpeur@gmail.com>
2022-08-04 21:10:02 +0200

    badge-reader/frontend git init

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,27 +1,48 @@
 <template lang="html">
   <div class="container">
-    <button @click.prevent="getTimers" class="btn btn-primary">
-      Send request
+    <div class="mb-4">
+      {{ this.data }}
+    </div>
+    <button @click.prevent="getDays" class="btn btn-primary">
+      Récupère la data
+    </button>
+    <button @click.prevent="postDay" class="btn btn-success">
+      Envoi la data
     </button>
   </div>
 </template>
 <script>
 export default {
   name: "HomeView",
+  data() {
+    return {
+      data: this.data,
+    };
+  },
   methods: {
-    getTimers() {
-      fetch("http://localhost:3000/api/timers")
+    getDays() {
+      fetch("http://localhost:3000/api/badge-time")
         .then((res) => {
           if (res.ok) {
             return res.json();
           }
         })
         .then((value) => {
-          console.log(value);
+          this.data = value;
         })
         .catch((err) => {
           console.log(err);
         });
+    },
+    postDay() {
+      fetch("http://localhost:3000/api/badge-time", {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({_id:'ndknezjkfkjze', userId: 'edenejfjzf'}),
+      });
     },
   },
 };
